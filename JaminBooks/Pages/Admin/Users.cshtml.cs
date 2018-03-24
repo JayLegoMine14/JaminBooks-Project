d54631c3f1f33c5a1f431d788e9fdb929c3b9425
James Allison <jaylegomanallison@gmail.com>
2018-03-24 12:38:43 -0500

    Added sample Users view for testing

---
+++ JaminBooks/Pages/Admin/Users.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JaminBooks.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JaminBooks.Pages.Admin
+{
+    public class UsersModel : PageModel
+    {
+        public IList<User> Users { get; private set; }
+
+        public void OnGet()
+        {
+            Users = new List<User>();
+            Users.Add(new User(1));
+        }
+
+        public void OnPostDelete(int id)
+        {
+            var user = new User(id);
+            user.Delete();
+        }
+    }
+}
