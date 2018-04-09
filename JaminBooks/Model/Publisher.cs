e6c2e2634c606292eca921a0bd95ae54b274c37f
Michael <flamekissedbacon@gmail.com>
2018-04-09 00:46:48 -0500

    Basically rewrote the Model by scratch. Added Rating. Will need to verify before claiming complete.

--- JaminBooks/Model/Publisher.cs
+++ JaminBooks/Model/Publisher.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Data;
 using static JaminBooks.Model.SQL;
+using System.Data.SqlTypes;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,113 +9,96 @@ using System.Threading.Tasks;
 namespace JaminBooks.Model
 {
     public class Publisher
-    {   
-        //Publisher fields
+    {
         public int PublisherID { private set; get; } = -1;
         public string PublisherName;
         public int AddressID;
-        public int PhoneID;
+        public int PhoneID;       
         public string ContactFirstName;
         public string ContactLastName;
-
-        //Address fields
-        public string Line1;
-        public string Line2;
-        public string City;
-        public string State;
-        public string Country;
-        public string ZIP;
-
-        //Phone fields
-        public string Number;
-        public string Category;
-
-        public Phone Phone = new Phone();
-        public Address Address = new Address();
+        public bool IsDeleted;
 
         public Publisher() { }
-
 
         public Publisher(int PublisherID)
         {
             DataTable dt = SQL.Execute("uspGetPublisherByID", new Param("PublisherID", PublisherID));
+
             if (dt.Rows.Count > 0)
             {
                 this.PublisherID = PublisherID;
-                this.PublisherName = (string)dt.Rows[0]["PublisherName"]; 
+                this.PublisherName = (String)dt.Rows[0]["PublisherName"];
                 this.AddressID = (int)dt.Rows[0]["AddressID"];
-                this.ContactFirstName = (string)dt.Rows[0]["ContactFirstName"];
-                this.ContactLastName = (string)dt.Rows[0]["ContactLastName"];
-                this.Phone = new Phone((int)dt.Rows[0]["PhoneID"]);
-                this.AddressID = ((int)dt.Rows[0]["AddressID"]);
+                this.PhoneID = (int)dt.Rows[0]["PhoneID"];
+                this.ContactFirstName = (String)dt.Rows[0]["ContactFirstName"];
+                this.ContactLastName = (String)dt.Rows[0]["ContactLastName"];
+                this.IsDeleted = (bool)dt.Rows[0]["IsDeleted"];
             }
             else
             {
-                throw new Exception("Invalid ID");
+                throw new Exception("Invalid Publisher ID");
             }
         }
 
+        private Publisher(int PublisherID, string PublisherName, int AddressID, int PhoneID,
+            string ContactFirstName, string ContactLastName, bool IsDeleted)
+        {
+            this.PublisherID = PublisherID;
+            this.PublisherName = PublisherName;
+            this.AddressID = AddressID;
+            this.PhoneID = PhoneID;
+            this.ContactFirstName = ContactFirstName;
+            this.ContactLastName = ContactLastName;
+            this.IsDeleted = IsDeleted;
+        }
 
         public void Save()
         {
-            {
-                Phone.Number = Number;
-                Phone.Category = Category;
-
-                Address.Line1 = Line1;
-                Address.Line2 = Line2;
-                Address.City = City;
-                Address.State = State;
-                Address.Country = Country;
-                Address.ZIP = ZIP;
-
-                Phone.Save();
-                Address.Save();
-                DataTable dt = SQL.Execute("uspSavePublisher",
-                new Param("PublisherName", PublisherName),
-                new Param("AddressID", Address.AddressID),
-                new Param("PhoneID", Phone.PhoneID),
-                new Param("ContactFirstName", ContactFirstName),
-                new Param("ContactLastName", ContactLastName));
+            DataTable dt = SQL.Execute("uspSavePublisher",
+               new Param("PublisherID", PublisherID),
+               new Param("PublisherName", PublisherName),
+               new Param("AddressID", AddressID),
+               new Param("PhoneID", PhoneID),
+               new Param("ContactFirstName", ContactFirstName),
+               new Param("ContactFirstName", ContactFirstName),
+               new Param("IsDeleted", IsDeleted));
 
 
-                if (dt.Rows.Count > 0)
-                {
-                    PublisherID = (int)dt.Rows[0]["PublisherID"];
 
-                }
-                else
-                {
-                    throw new Exception("Book Not Created");
-                }
-
-             }
-        }
-
-        public int GetPublisherIDByName()
-        {
-            DataTable dt = SQL.Execute("uspGetPublisherIDByName",
-                new Param("PublisherName", PublisherName));
-
-
-            if (dt.Rows.Count == 0)
-            {
-                Save();
-            }
-            else
-            {
+            if (dt.Rows.Count > 0)
                 PublisherID = (int)dt.Rows[0]["PublisherID"];
-
-            }
-
-            return PublisherID;
         }
 
-
-
-        public void delete()
+        public void Delete()
         {
-            DataTable dt = SQL.Execute("uspDeletePublisher",
+            DataTable dt = SQL.Execute("uspDeletePublisher", new Param("PublisherID", PublisherID));
+            IsDeleted = true;
+        }
+
+        public int GetBookID()
+        {
+            DataTable dt = SQL.Execute("uspGetPublisherByID",
+                new Param("PublisherID", PublisherID));
+            return (int)dt.Rows[0]["PublisherID"];
+        }
+
+        //These two need to be looked at
+        public void AddAddress(Address a)
+        {
+            a.AddUser(this.PublisherID);
+        }
+        
+        public void AddPhone(Phone p)
+        {
+            p.AddUser(this.PublisherID);
+        }
+        //these two need to be looked at
+
+
+        public void AddPublisher(int BookID)
+        {
+            DataTable dt = SQL.Execute("uspBookAddPublisher",
+                new Param("BookID", BookID),
                 new Param("PublisherID", PublisherID));
         }
     }
