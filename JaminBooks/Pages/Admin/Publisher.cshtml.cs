37378489dea57948b88ebc1dc0d9339aca1e5451
James Allison <jaylegomanallison@gmail.com>
2018-04-24 09:37:59 -0500

    Added Publisher page

---
+++ JaminBooks/Pages/Admin/Publisher.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JaminBooks.Model;
+using JaminBooks.Tools;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JaminBooks.Pages.Admin
+{
+    public class PublisherModel : PageModel
+    {
+        public User CurrentUser;
+        public Publisher Publisher; 
+
+        public void OnGet(int? id)
+        {
+            CurrentUser = Authentication.GetCurrentUser(HttpContext);
+            if (CurrentUser == null || !CurrentUser.IsAdmin)
+            {
+                Response.Redirect("/");
+            }
+
+            Publisher = id != null ? new Publisher(id.Value) : new Publisher();
+        }
+    }
+}
